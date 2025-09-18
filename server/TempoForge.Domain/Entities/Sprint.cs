2f1edf8b4175ad14bfaea44214501043f360c9a3
Travinkel <ankersoe@protonmail.com>
2025-09-18 15:33:02 +0200

    feat(domain): add Sprint entity

---
+++ server/TempoForge.Domain/Entities/Sprint.cs
@@ -0,0 +1,21 @@
+﻿namespace TempoForge.Domain.Entities;
+
+public enum SprintStatus
+{
+    Running = 1,
+    Completed = 2,
+    Aborted = 3
+}
+
+public class Sprint
+{
+    public Guid Id { get; set; }
+    public Guid ProjectId { get; set; }
+    public Project? Project { get; set; }
+    public int DurationMinutes { get; set; }
+    public DateTime StartedAt { get; set; }
+    public DateTime? CompletedAt { get; set; }
+    public DateTime? AbortedAt { get; set; }
+    public SprintStatus Status { get; set; }
+}
+
